595163984a8b2d643db9a73f97f46599c101fa64
zml1997-stl <str8twojunk@gmail.com>
2025-03-08 22:26:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def root():
-    return {"message": "Welcome to WordRush!"}
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
